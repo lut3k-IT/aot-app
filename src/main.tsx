28008d7d514b5420d6c8da93a41f786cb7219729
Kacper Lutyński <lut3k777@gmail.com>
2023-09-25 13:03:07 +0200

    all files linted

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import { ThemeProvider } from './components/ui/theme-provider';
+
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
