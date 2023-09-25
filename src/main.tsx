4e1b761bffcc907d50af7b5a77cb871d702a9fc5
Kacper Lutyński <lut3k777@gmail.com>
2023-09-25 12:54:46 +0200

    prettier tailwind eslint configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { ThemeProvider } from './components/ui/theme-provider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider
+      defaultTheme='dark'
+      storageKey='vite-ui-theme'
+    ></ThemeProvider>
+  </React.StrictMode>
+);
