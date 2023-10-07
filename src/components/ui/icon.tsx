cfdfce4767436bf63fc456777b34f466cb72157d
Kacper Lutyński <lut3k777@gmail.com>
2023-10-07 20:41:53 +0200

    icon component added

--- src/components/ui/icon.tsx
+++ src/components/ui/icon.tsx
@@ -1,30 +1,95 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { ReactComponent as CloseIcon } from 'src/assets/icons/x.svg';
+import React from 'react';
+import { cva } from 'class-variance-authority';
+import {
+  ArrowDownWideNarrow,
+  Dna,
+  Filter,
+  HelpCircle,
+  Loader2,
+  LucideIcon,
+  Menu,
+  Moon,
+  PenLine,
+  Sun,
+  User,
+  X
+} from 'lucide-react';
 
-export type IconNames = 'close';
+import { cn } from '@/lib/utils';
 
-interface IconProps extends Partial<HTMLAnchorElement> {
+const iconVariants = cva('text-current', {
+  variants: {
+    size: {
+      sm: 'w-5 h-5',
+      md: 'w-6 h-6',
+      lg: 'w-7 h-7'
+    }
+  },
+  defaultVariants: {
+    size: 'md'
+  }
+});
+
+type IconSizes = 'sm' | 'md' | 'lg';
+
+type IconNames =
+  | 'arrowDownWideNarrow'
+  | 'dna'
+  | 'filter'
+  | 'helpCircle'
+  | 'loader2'
+  | 'menu'
+  | 'moon'
+  | 'penLine'
+  | 'sun'
+  | 'user'
+  | 'x';
+
+type Icons = Record<IconNames, React.ElementType>;
+
+interface IconProps extends Partial<LucideIcon> {
   name: IconNames;
-  // link?: string;
-  small?: boolean; //or change to size
-  svgProps?: object;
+  size?: IconSizes;
+  color?: string;
+  isFilled?: boolean;
+  className?: string;
 }
 
-const Icon = (props: IconProps) => {
-  const { name, svgProps } = props;
-
-  const iconProps = {
-    // className:, // allow to add various tailwind variants
-    ...svgProps
-  };
-
-  const Icons: Record<IconProps['name'], any> = {
-    close: CloseIcon
+const Icon = ({ name, size, color, isFilled = false, className, ...props }: IconProps) => {
+  const Icons: Icons = {
+    arrowDownWideNarrow: ArrowDownWideNarrow,
+    dna: Dna,
+    filter: Filter,
+    helpCircle: HelpCircle,
+    loader2: Loader2,
+    menu: Menu,
+    moon: Moon,
+    penLine: PenLine,
+    sun: Sun,
+    user: User,
+    x: X
   };
   const IconComponent = Icons[name];
 
-  return <IconComponent {...iconProps} />;
+  const iconClass = cn(
+    iconVariants({
+      size: size,
+      className
+    })
+  );
+
+  const iconColor = color ? color : 'currentColor';
+
+  return (
+    <IconComponent
+      className={iconClass}
+      color={iconColor}
+      fill={isFilled ? iconColor : null}
+      {...props}
+    />
+  );
 };
 
 export default Icon;
