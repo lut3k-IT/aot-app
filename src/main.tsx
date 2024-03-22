887a682457d6d52ccfe7ae793b4cd5b2e542fbe3
Kacper Lutyński <lut3k777@gmail.com>
2024-03-22 17:24:51 +0100

    light theme default

--- src/main.tsx
+++ src/main.tsx
@@ -19,7 +19,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <I18nextProvider i18n={i18next}>
         <HelmetProvider>
           <ThemeProvider
-            defaultTheme='dark'
+            defaultTheme='light'
             storageKey='vite-ui-theme'
           >
             <AppRouter />
