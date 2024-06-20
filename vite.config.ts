022ec350208a90f2740dd218b7ee5a084138ef49
Kacper Lutyński <lut3k777@gmail.com>
2024-06-20 19:04:06 +0200

    chore(deps): update Vite

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,13 @@ import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: { exportType: 'named', ref: true, svgo: false, titleProp: true },
+      include: '**/*.svg'
+    })
+  ],
   define: {
     APP_VERSION: JSON.stringify(process.env.npm_package_version)
   },
