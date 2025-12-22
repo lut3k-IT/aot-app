350857a250e344aa660b0ee7b26d37510378f4d0
Kacper Lutyński <lut3k777@gmail.com>
2025-12-23 00:57:12 +0100

    feat: implement PWA

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import { VitePWA } from 'vite-plugin-pwa';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
@@ -20,6 +21,37 @@ export default defineConfig({
       },
       png: {
         quality: 80
+      }
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'AOT APP',
+        short_name: 'AOT',
+        description: "Explore the world of 'Attack on Titan' better with the AOT APP!",
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'assets/icons/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'assets/icons/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'assets/icons/aot-icon.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+            purpose: 'any maskable'
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true
       }
     })
   ],
